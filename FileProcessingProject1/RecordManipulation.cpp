6becd2abd6ba810a4e2aee71f3e0ec3443a231b6
seastar105 <seastar105@gmail.com>
2018-10-09 22:01:22 +0900

    Search Done

--- FileProcessingProject1/RecordManipulation.cpp
+++ FileProcessingProject1/RecordManipulation.cpp
@@ -12,12 +12,12 @@ using namespace std;
 
 void make_memories() {
 	char f1[] = "fileOfMember.dat";
-	char f2[] = "fileOfLecutre.dat";
+	char f2[] = "fileOfLecture.dat";
 	char f3[] = "fileOfPurchase.dat";
 	DelimFieldBuffer buf1('|'), buf2('|'), buf3('|');
 	RecordFile<Member> MemberFile(buf1);
-	RecordFile<Purchase> PurchaseFile(buf2);
-	RecordFile<Lecture> LectureFile(buf3);
+	RecordFile<Lecture> LectureFile(buf2);
+	RecordFile<Purchase> PurchaseFile(buf3);
 
 	Member M;
 	MemberFile.Open(f1, ios::in);
@@ -84,13 +84,15 @@ void clear_console() {
 	system("cls");
 }
 
-int record_type() {
+int record_type(string action) {
 	bool exit_flag = false;
 
 	string buf;
 	int type;
 		while (!exit_flag) {
 			clear_console();
+			cout << "******************************" << endl;
+			cout << "* " << action << " Record              *" << endl;
 			cout << "******************************" << endl;
 			cout << "* Choose Record Type         *" << endl;
 			cout << "******************************" << endl;
@@ -111,7 +113,6 @@ int record_type() {
 				continue;
 			}
 			else {
-				clear_console();
 				return type;
 			}
 		}
@@ -131,6 +132,7 @@ void purchase_menu() {
 
 void LecturePurchaseSystem() {
 	bool exit_flag=false;
+	bool action_flag = false;
 	while (!exit_flag) {
 		string buf;
 		int selection;
@@ -143,14 +145,73 @@ void LecturePurchaseSystem() {
 		else selection = -1;
 		switch (selection) {									// 1:search, 2:insert, 3:delete, 4:update,5:exit
 		case 1:
-			record_search(); break;
+			action_flag = true; record_search(); break;
 		case 2:
-			record_insert(); break;
+			action_flag = true; record_insert(); break;
 		case 3:
-			record_delete(); break;
+			action_flag = true; record_delete(); break;
 		case 4:
-			record_update(); break;
+			action_flag = true; record_update(); break;
 		case 5:
+			exit_flag = true; break;
+		default:
+			cout << endl << "Please Input Correctly" << endl; break;
+		}
+		if (!action_flag) {
+			action_flag = true;
+			cout << endl << "If you want to CONTINUE, press Enter..." << endl;
+			cin.get();
+			clear_console();
+		}
+	}
+}
+
+void record_search() {
+	bool exit_flag = false;
+	bool type_flag = false;
+	vector<int> idx;
+	make_memories();
+	while (!exit_flag) {
+		string search_key;
+		int type;
+		clear_console();
+		type = record_type("Search");
+		switch (type) {
+		case 1:
+			cout << endl << " Input Member ID : ";
+			cin >> search_key;
+			cin.get();
+			if (search_member(search_key)) {
+				cout << endl << Members.find(search_key)->second;
+			}
+			else
+				cout << endl << "There's no such Member" << endl;
+			break;
+		case 2:
+			cout << endl << " Input Lecture ID : ";
+			cin >> search_key;
+			cin.get();
+			if (search_lecture(search_key)) {
+				cout << endl << Lectures.find(search_key)->second;
+			}
+			else
+				cout << endl << "There's no such Lecture" << endl;
+			break;
+		case 3:
+			cout << endl << " Input Key to search Purchase : ";
+			cin >> search_key;
+			cin.get();
+			idx = search_purchase(search_key);
+			if (idx.size() != 0) {
+				cout << endl;
+				for (auto it = idx.begin(); it != idx.end(); it++) {
+					cout << Purchases.at(*it);
+				}
+			}
+			else
+				cout << endl << "There's no Suct Purchase" << endl;
+			break;
+		case 4:
 			exit_flag = true; break;
 		default:
 			cout << endl << "Please Input Correctly" << endl; break;
@@ -159,10 +220,42 @@ void LecturePurchaseSystem() {
 		cin.get();
 		clear_console();
 	}
+	delete_memories();
 }
 
-void record_search() {
+bool search_member(string key) {
+	if (Members.find(key) != Members.end())
+		return true;
+	else
+		return false;
+}
 
+bool search_lecture(string key) {
+	if (Lectures.find(key) != Lectures.end())
+		return true;
+	else
+		return false;
+}
+
+vector<int> search_purchase(string key) {
+	vector<int> ret;
+	int i = 0;
+	for (auto it = Purchases.begin(); it != Purchases.end(); it++,i++) {
+		if (!key.compare(it->get_PID()))
+			ret.push_back(i);
+		else if (!key.compare(it->get_MID()))
+			ret.push_back(i);
+		else if (!key.compare(it->get_LID()))
+			ret.push_back(i);
+	}
+	return ret;
+}
+
+bool search_PID(string key) {
+	for (auto it = Purchases.begin(); it != Purchases.end(); it++)
+		if (!key.compare(it->get_PID()))
+			return true;
+	return false;
 }
 
 void record_insert() {
--- FileProcessingProject1/RecordManipulation.h
+++ FileProcessingProject1/RecordManipulation.h
@@ -14,12 +14,19 @@
 void clear_console();			// use system command to clear console (works only with windows)
 void LecturePurchaseSystem();
 void purchase_menu();
-int record_type();
+int record_type(string);
 
 void record_search();
 void record_insert();
 void record_delete();
 void record_update();
+
 void make_memories();
 void delete_memories();
+
+bool search_member(string);
+bool search_lecture(string);
+vector<int> search_purchase(string);
+
+bool search_PID(string);
 #endif
